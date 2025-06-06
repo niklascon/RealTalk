807e9be022c300ac004a918ad6b80922a3ab589b
Niklas <niklas.schulz@stud.tu-darmstadt.de>
2025-06-06 15:09:06 +0200

    added some more words

--- menubar_app.py
+++ menubar_app.py
@@ -8,7 +8,19 @@ script_path = os.path.dirname(__file__)
 rt_icon_path = "assets/icons/Realtalk_transparent.png"
 rt_icon = os.path.join(script_path, rt_icon_path)
 
-KEYWORDS = ["hallo", "exit", "hilfe"]
+KEYWORDS = ["exit", "scam", "scammen", "betrug", "passwort", "passwörter", "daten", "datei", "geld", "money", "finanzen",
+"enkel", "enkelkind", "kind", "konto", "kontonummer", "iban", "bic", "bank", "überweisung", "onlinebanking", "bankdaten", "bankverbindung",
+"zugangsdaten", "tan", "photo-tan", "push-tan", "chip-tan", "banking-app", "bankkonto", "sicherheitscode", "pin", "passwort", "passwörter", "login", "zugang", "authentifizierung", "verifizierung", "identität",
+"zugriffsrechte", "sicherheitsfrage", "freigabe", "sperrung", "entsperren", "token", "code", "sms-code", "enkel", "enkelkind", "oma", "opa", "kind", "tochter", "sohn", "verwandt", "familie", "notfall", "dringend",
+"unfall", "hilfe", "verhaftet", "kaution", "geld", "überweisen", "zahlung", "zahlen", "betrag", "scam", "scammen", "betrug", "betrüger", "kriminell", "abzocke", "erpressung", "dringend", "sofort", "gefahr", "notfall", "schaden", "verlust", "support", "it-abteilung", "technik", "update", "fernzugriff", "teamviewer", "anydesk", "zugriff", "installieren",
+"computer", "wartung", "sicherheitslücke", "hacker", "viren", "trojaner", "zugang geben", "daten", "datei", "dateien", "dokument", "dokumente", "ausweis", "personalausweis", "führerschein",
+"kopie", "scan", "hochladen", "identitätsnachweis", "unterlagen", "persönlich", "vertraulich", "polizei", "kripo", "staatsanwaltschaft", "gericht", "finanzamt", "bundesbank", "ezb", "zoll", "behörde",
+"regierung", "botschaft", "konsulat", "ermittlung", "verhör", "anzeige", "aktenzeichen", "anwalt", "notar", "beamter", "bankmitarbeiter", "berater", "microsoft", "it-support", "telekom", "versicherer",
+"post", "inkasso", "stromanbieter", "internetprovider", "kundenservice", "kundendienst", "servicecenter", "dringlichkeit", "sofort", "wichtig", "vertraulich", "zeitdruck", "frist", "mahnbescheid", "pfändung",
+"geheim", "verheimlichen", "nicht erzählen", "keinem sagen", "nur du", "vertraue mir", "niemandem mitteilen", "dhl", "ups", "gls", "sendung", "paket", "zustellung", "versand", "lieferung", "versandkosten", "tracking",
+"lieferprobleme", "paketdienst", "formular", "link", "klick", "website", "eingeben", "bestätigen", "verifizieren", "konto bestätigen",
+"zahlung autorisieren", "weiterleitung", "eingabemaske", "anmeldung", "captcha", "online-zugang", "whatsapp", "telegram", "nachricht", "sms", "code erhalten", "code weiterleiten", "neue nummer", "alte nummer verloren",
+"handy kaputt", "neue sim", "instagram", "facebook", "profil gehackt", "account gesperrt"]
 
 class MenuBarApp(rumps.App):
     def __init__(self):
