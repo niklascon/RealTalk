3f43e5070c8671ea2588bee6616664975c2b1c54
Niklas <niklas.schulz@stud.tu-darmstadt.de>
2025-06-11 13:00:40 +0200

    changed icons

--- menubar_app.py
+++ menubar_app.py
@@ -5,21 +5,22 @@ import queue
 from common.speech import recognize_speech
 
 script_path = os.path.dirname(__file__)
-rt_icon_path_listening = "assets/icons/Realtalk_transparent_green.png"
-rt_icon_path_paused = "assets/icons/Realtalk_transparent_red.png"
+rt_icon_path_listening = "assets/icons/RealTalk_transparent_green.png"
+rt_icon_path_paused = "assets/icons/RealTalk_transparent_red.png"
+rt_icon_path_white = "assets/icons/RealTalk_transparent_white.png"
 rt_icon = os.path.join(script_path, rt_icon_path_paused)
 
-KEYWORDS = ["exit", "scam", "scammen", "betrug", "passwort", "passwörter", "daten", "datei", "geld", "money", "finanzen",
+KEYWORDS = ["exit", "scam", "scammen", "betrug", "passwort", "passwörter", "geld", "money", "finanzen",
 "enkel", "enkelkind", "kind", "konto", "kontonummer", "iban", "bic", "bank", "überweisung", "onlinebanking", "bankdaten", "bankverbindung",
 "zugangsdaten", "tan", "photo-tan", "push-tan", "chip-tan", "banking-app", "bankkonto", "sicherheitscode", "pin", "passwort", "passwörter", "login", "zugang", "authentifizierung", "verifizierung", "identität",
-"zugriffsrechte", "sicherheitsfrage", "freigabe", "sperrung", "entsperren", "token", "code", "sms-code", "enkel", "enkelkind", "oma", "opa", "kind", "tochter", "sohn", "verwandt", "familie", "notfall", "dringend",
-"unfall", "hilfe", "verhaftet", "kaution", "geld", "überweisen", "zahlung", "zahlen", "betrag", "scam", "scammen", "betrug", "betrüger", "kriminell", "abzocke", "erpressung", "dringend", "sofort", "gefahr", "notfall", "schaden", "verlust", "support", "it-abteilung", "technik", "update", "fernzugriff", "teamviewer", "anydesk", "zugriff", "installieren",
-"computer", "wartung", "sicherheitslücke", "hacker", "viren", "trojaner", "zugang geben", "daten", "datei", "dateien", "dokument", "dokumente", "ausweis", "personalausweis", "führerschein",
+"zugriffsrechte", "sicherheitsfrage", "freigabe", "sperrung", "entsperren", "token", "code", "sms-code", "enkel", "enkelkind", "oma", "opa", "kind", "tochter", "sohn", "verwandt", "familie", "dringend",
+"unfall", "verhaftet", "kaution", "geld", "überweisen", "zahlung", "zahlen", "betrag", "scam", "scammen", "betrug", "betrüger", "kriminell", "abzocke", "erpressung", "dringend", "sofort", "gefahr", "notfall", "schaden", "verlust", "support", "it-abteilung", "technik", "update", "fernzugriff", "teamviewer", "anydesk", "zugriff", "installieren",
+"computer", "wartung", "sicherheitslücke", "hacker", "viren", "trojaner", "zugang geben", "daten", "ausweis", "personalausweis", "führerschein",
 "kopie", "scan", "hochladen", "identitätsnachweis", "unterlagen", "persönlich", "vertraulich", "polizei", "kripo", "staatsanwaltschaft", "gericht", "finanzamt", "bundesbank", "ezb", "zoll", "behörde",
 "regierung", "botschaft", "konsulat", "ermittlung", "verhör", "anzeige", "aktenzeichen", "anwalt", "notar", "beamter", "bankmitarbeiter", "berater", "microsoft", "it-support", "telekom", "versicherer",
 "post", "inkasso", "stromanbieter", "internetprovider", "kundenservice", "kundendienst", "servicecenter", "dringlichkeit", "sofort", "wichtig", "vertraulich", "zeitdruck", "frist", "mahnbescheid", "pfändung",
 "geheim", "verheimlichen", "nicht erzählen", "keinem sagen", "nur du", "vertraue mir", "niemandem mitteilen", "dhl", "ups", "gls", "sendung", "paket", "zustellung", "versand", "lieferung", "versandkosten", "tracking",
-"lieferprobleme", "paketdienst", "formular", "link", "klick", "website", "eingeben", "bestätigen", "verifizieren", "konto bestätigen",
+"lieferprobleme", "paketdienst", "link", "verifizieren", "konto bestätigen",
 "zahlung autorisieren", "weiterleitung", "eingabemaske", "anmeldung", "captcha", "online-zugang", "whatsapp", "telegram", "nachricht", "sms", "code erhalten", "code weiterleiten", "neue nummer", "alte nummer verloren",
 "handy kaputt", "neue sim", "instagram", "facebook", "profil gehackt", "account gesperrt"]
 
@@ -95,7 +96,7 @@ class MenuBarApp(rumps.App):
                 title="RealTalk",
                 message="RealTalk has identified suspicious voice activity. Please end the call immediately and notify your security team.",
                 ok="Acknowledge",
-                icon_path=rt_icon_path
+                icon_path=rt_icon_path_white
             )
 
 
